75d72da3f714438dca6b7b4eec26db94f22f6be1
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-07-13 21:55:38 +0530

    day 4

---
+++ Mercer Mettl/Non-Repeated Digits’ Count/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+    public int Solution(int input1) {
+        // Read only region end
+        // Write code here...
+        int[] uDigits = new int[10];
+        int count = 0;
+        
+        while (input1 >= 1) {
+            int digit = input1 % 10;
+            if (uDigits[digit] == 0) {
+                uDigits[digit] = 1;
+                count++;
+            } else if (uDigits[digit] == 1) {
+                uDigits[digit] = -1;
+                count--;
+            } 
+            input1 /= 10;
+            
+        }
+
+        return count;
+
+        // throw new UnsupportedOperationException("IsEven(int input1)");
+    }
+}
